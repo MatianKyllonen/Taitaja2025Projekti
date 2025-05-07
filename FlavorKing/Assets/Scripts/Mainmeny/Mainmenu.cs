68c223efa52ad5e3a7e45fb8b26a5b743408cb66
osouk <olavi.soukainen@gmail.com>
2025-05-07 16:39:24 +0300

    7.5 korjaa sä

--- FlavorKing/Assets/Scripts/Mainmeny/Mainmenu.cs
+++ FlavorKing/Assets/Scripts/Mainmeny/Mainmenu.cs
@@ -1,20 +1,67 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+
 
 public class Mainmenu : MonoBehaviour
 {
-    public void Playgame()
+    public GameObject mainmeny;
+    public GameObject tutorial;
+
+    public List<GameObject> tutorialImages;
+    private int currentImageIndex = 0;
+
+    public void Start()
     {
-        SceneManager.LoadScene("SampleScene");
+        mainmeny.SetActive(true);
+        tutorial.SetActive(false);
+        HideAllTutorialImages();
     }
 
-    public void Settings()
+    public void Playgame()
     {
-        SceneManager.LoadScene("SettingsMenu");
+        mainmeny.SetActive(false);
+        tutorial.SetActive(true);
+        ShowImage(0); // show first image
     }
-    
+
+    public void NextImage()
+{
+    if (currentImageIndex < tutorialImages.Count - 1)
+    {
+        tutorialImages[currentImageIndex].SetActive(false);
+        currentImageIndex++;
+        tutorialImages[currentImageIndex].SetActive(true);
+    }
+    else
+    {
+        tutorialImages[currentImageIndex].SetActive(false); // hide last image
+        Debug.Log("done");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
+
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private void ShowImage(int index)
+    {
+        HideAllTutorialImages();
+        if (index >= 0 && index < tutorialImages.Count)
+        {
+            tutorialImages[index].SetActive(true);
+            currentImageIndex = index;
+        }
+    }
+
+    private void HideAllTutorialImages()
+    {
+        foreach (var img in tutorialImages)
+        {
+            img.SetActive(false);
+        }
     }
 }
